8401b8d31d1a25cd52cd3014d3067007a1a6fd28
Gabriel Dougherty <contact@gabrieldougherty.com>
2020-12-29 02:06:43 -0500

    Persist state and restore drawing on connect

--- app.js
+++ app.js
@@ -1,29 +1,33 @@
 "use strict";
-var app = require('express')();
-var server = require('http').Server(app);
-var io = require('socket.io')(server);
+const path = require('path');
+const app = require('express')();
+const server = require('http').Server(app);
+const io = require('socket.io')(server);
 
 server.listen(3004);
 // WARNING: app.listen(80) will NOT work here!
 
 app.get('/', function (req, res) {
-  res.sendFile(__dirname + '/index.html');
+    res.sendFile(path.join(__dirname, 'index.html'));
 });
 
 app.get('/canvas.js', (req, res) => {
-    res.sendFile(__dirname + '/canvas.js');
+    res.sendFile(path.join(__dirname, 'canvas.js'));
 });
-var points = [];
+var allPoints = [];
 var id = 0;
 io.on('connection', function (socket) {
-    socket.emit('id', {id: id});
+    socket.emit('id', { id: id });
+    if (allPoints.length > 0) {
+        socket.emit('newpoints', { id: -1, points: allPoints });
+    }
     id++;
     socket.on('points', function (data) {
         if (data.id !== -1) {
-            points.concat(data.points);
+            allPoints = allPoints.concat(data.points);
         }
         console.log('ids', data.id);
         console.log("got points", data.points);
-        socket.broadcast.emit('new points', data);
+        socket.broadcast.emit('newpoints', data);
     });
 });
--- canvas.js
+++ canvas.js
@@ -5,41 +5,41 @@ const canvas = document.getElementById('drawingCanvas'),
     canvTop = canvas.offsetTop;
 var socket = io();
 var id = -1;
-var mouseProperties = {state: 'up'}
+var mouseProperties = { state: 'up' }
 var ctx;
 var points = [];
 socket.on('id', (data) => id = data.id);
 
 // send points to server
-const sendPoints = function(points) {
+const sendPoints = function (points) {
     console.log(`Sending ${points.length} points`);
     console.log(points);
-    socket.emit('points', {id: id, points: points});
+    socket.emit('points', { id: id, points: points });
     points.length = 0;
 };
-const drawPoint = function(point) {
-    ctx.fillRect(point.x,point.y,5,5);
+const drawPoint = function (point) {
+    ctx.fillRect(point.x, point.y, 5, 5);
 };
-const getCurrentPoint = function(event) {
+const getCurrentPoint = function (event) {
     let x = event.pageX - canvLeft,
         y = event.pageY - canvTop;
-    return {x,y};
+    return { x, y };
 };
-const drawCurrentPoint = function(event) {
+const drawCurrentPoint = function (event) {
     let point = getCurrentPoint(event);
     drawPoint(point);
     points.push(point);
 };
-const handleMouseMove = function(event) {
+const handleMouseMove = function (event) {
     if (mouseProperties.state === 'down') {
         drawCurrentPoint(event);
     }
 };
-const handleMouseDown = function(event) {
+const handleMouseDown = function (event) {
     drawCurrentPoint(event);
     mouseProperties.state = 'down';
 };
-const handleMouseUp = function(event) {
+const handleMouseUp = function (event) {
     drawCurrentPoint(event);
     mouseProperties.state = 'up';
     sendPoints(points);
@@ -53,11 +53,11 @@ if (canvas.getContext) {
     canvas.addEventListener('touchend', handleMouseUp);
     canvas.addEventListener('touchstart', handleMouseDown);
     canvas.addEventListener('touchmove', handleMouseMove);
-    socket.on('new points', (data) => {
+    socket.on('newpoints', (data) => {
         console.log('got new points', data.id, data.points);
         if (data.id !== id) {
             for (let p of data.points) {
-                ctx.fillRect(p.x,p.y,5,5);
+                ctx.fillRect(p.x, p.y, 5, 5);
             }
         }
     });
